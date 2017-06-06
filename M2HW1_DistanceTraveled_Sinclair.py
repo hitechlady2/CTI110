72c0cfddfe8229628374954d5236c029b462c5cb
hitechlady2 <sinclaib1491@student.faytechcc.edu>
2017-06-06 02:15:01 -0400

    Add files via upload
    Distance travel python code

---
+++ M2HW1_DistanceTraveled_Sinclair.py
@@ -0,0 +1,31 @@
+#Distance Traveled Assignment Page 77 Exercise#5
+#06/05/17
+#CTI-110 M2HW1 Distance Traveled
+#Benescha Sinclair
+
+#Distance a car travels at 70 mph in hours.
+print('The distance the car will travel in 6 hours is')
+
+#Calculate the distance traveled.
+distance = 6 * 70
+
+#Display the distance
+print(distance, 'miles')
+
+#Distance a car travels at 70 mph in 10 hours.
+print('The distance the car will travel in 10 hours is')
+
+#Display the distance
+distance = 10 * 70
+
+#Display the distance
+print(distance, 'miles')
+
+#Distance a car travels at 70 mph in 15 hours.
+print('The distance the car will travel in 15 hours is')
+
+#Display the distance
+distance = 15 * 70
+
+#Display the distance
+print(distance, 'miles')
