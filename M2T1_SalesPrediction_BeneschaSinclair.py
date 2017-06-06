47e97ee4914b01ca695cf0251eb2d8335db86a19
hitechlady2 <sinclaib1491@student.faytechcc.edu>
2017-06-06 02:11:50 -0400

    Add files via upload
    First upload of my sales prediction code.

---
+++ M2T1_SalesPrediction_BeneschaSinclair.py
@@ -0,0 +1,14 @@
+#Sales Prediction Assignment
+#06/05/2017
+#CTI-110 M2T1- Sales Prediction
+#Benescha Sinclair
+
+#Get the projected total sales.
+total_sales = float(input('Enter the Projected Sales: '))
+
+#Calculate the profit as 23 precent of total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print ('The profit is $', format(profit, ',.2f'))
+
