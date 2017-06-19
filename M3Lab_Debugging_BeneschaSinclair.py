06eeb92afe34f5821afd4c73a6bce9bf04728147
Benescha Sinclair <sinclaib1491@student.faytechcc.edu>
2017-06-19 12:06:15 -0500

    Add files via upload
    debugging

---
+++ M3Lab_Debugging_BeneschaSinclair.py
@@ -0,0 +1,17 @@
+#M3LAB Debugging (Exercise #4, pg 115)
+#06/18/17
+#CTI-110 M3LAB Debugging
+#Benescha Sinclair
+
+#Rewrite the code and use the proper conventions of alignment and indentation.
+
+if score >= A_score:
+    print("Your grade is A." )
+elif score >= B_score:
+    print("Your grade is B." )
+elif score >= C_score:
+    print("Your grade is C." )
+elif score >= D_score:
+    print("Your grade is D." )
+else: 
+    print("Your grade is F." )
